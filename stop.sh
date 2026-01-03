6936ca5e34a15c8d15a33adaeb0ecafe4733baf9
renshiro hata <135189083+lat-pulldown@users.noreply.github.com>
2026-01-03 18:06:05 +0900

    Delete stop.sh

--- stop.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-echo "[+] Stopping DMZ services"
-
-cd ~/conpot && docker-compose down
-cd ~/thingsboard && docker-compose down
-
-echo "[+] CALDERA must be stopped manually (Ctrl+C)"
